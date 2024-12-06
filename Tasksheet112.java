55cd78af9c81701821c24a6b993004c9fad631e3
Franzene <126221941+zeesaa@users.noreply.github.com>
2024-12-06 19:41:24 +0800

    Task sheet 112

---
+++ Tasksheet112.java
@@ -0,0 +1,12 @@
+public class Tasksheet112 {
+    public static void main(String[] args) {
+        byte zero = 0;
+        int myInt = 3110;
+        short myShort = 1;
+        char myChar = 'd';
+        float myFloat = 2.0f;
+        boolean myBool = true;
+        String output = "H" + myInt + " w" + zero + "r" + myShort + myChar + " "+ myFloat + " " + myBool;
+        System.out.println(output);
+    }
+}
